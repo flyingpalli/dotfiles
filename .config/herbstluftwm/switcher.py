fdc67e39e18dfe179f45d9bc169e377aec0aef93
flyingpalli <paskal.thomas@t-online.de>
2024-04-15 21:51:05 +0200

    add mirror mode and improve switcher

--- .config/herbstluftwm/switcher.py
+++ .config/herbstluftwm/switcher.py
@@ -5,45 +5,27 @@ from argparse import ArgumentParser
 from typing import Any, Dict, Optional
 
 
-def check_third_line(value):
-    try:
-        with open("autostart", "r") as file:
-            lines = [next(file) for _ in range(3)]
-            if value in lines[2]:
-                return True
-                print("i returned true")
-            else:
-                return False
-                print("i returned false")
-    except FileNotFoundError:
-        print("something went horribly wrong")
-        return
-
-
 def main():
     parser = ArgumentParser(prog="switcher.py")
     parser.add_argument("value", type=str)
     args = parser.parse_args()
 
+    if args.value not in ["laptop", "desktop", "mirror"]:
+        return
+
     if args.value == "laptop":
         subprocess.run(
             args=["python", "/home/pth/pallisupercoding/fontsize/fontsize.py", "10"]
         )
-    elif args.value == "desktop":
+    elif args.value in ["mirror", "desktop"]:
         subprocess.run(
             args=["python", "/home/pth/pallisupercoding/fontsize/fontsize.py", "11"]
         )
 
-    if check_third_line(args.value):
-        return
-    os.rename("autostart", "temp_autostart")
-    os.rename("other_autostart", "autostart")
-    os.rename("temp_autostart", "other_autostart")
+    os.system(f"cp {args.value}_autostart autostart")
 
     os.chdir("/home/pth/.config/rofi")
-    os.rename("config.rasi", "temp_config.rasi")
-    os.rename("other_config.rasi", "config.rasi")
-    os.rename("temp_config.rasi", "other_config.rasi")
+    os.system(f"cp {args.value}_config.rasi config.rasi")
 
 
 if __name__ == "__main__":
