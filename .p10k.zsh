5fce49d156cf161c65f957bb7c6f9ea49cd590c0
flyingpalli <paskal.thomas@t-online.de>
2024-10-13 23:40:48 +0200

    some color stuff

--- .p10k.zsh
+++ .p10k.zsh
@@ -207,14 +207,14 @@
 
   ##################################[ dir: current directory ]##################################
   # Default current directory color.
-  typeset -g POWERLEVEL9K_DIR_FOREGROUND='#7f849c'
+  typeset -g POWERLEVEL9K_DIR_FOREGROUND='#89b4fa'
   # If directory is too long, shorten some of its segments to the shortest possible unique
   # prefix. The shortened directory can be tab-completed to the original.
   typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_unique
   # Replace removed segment suffixes with this symbol.
   typeset -g POWERLEVEL9K_SHORTEN_DELIMITER=
   # Color of the shortened directory segments.
-  typeset -g POWERLEVEL9K_DIR_SHORTENED_FOREGROUND='#7f849c'
+  typeset -g POWERLEVEL9K_DIR_SHORTENED_FOREGROUND='#89b4fa'
   # Color of the anchor directory segments. Anchor segments are never shortened. The first
   # segment is always an anchor.
   typeset -g POWERLEVEL9K_DIR_ANCHOR_FOREGROUND='#89b4fa'
